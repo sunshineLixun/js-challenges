2dc1f35e2a940d17ac62b075802c9d83a8a0bb74
Hello Li <1261142605@qq.com>
2023-07-07 17:54:44 +0800

    add vitest

--- src/promise/test/promise.all.test.js
+++ src/promise/test/promise.all.test.js
@@ -1,42 +1,6 @@
 import { describe, expect, test } from "vitest";
 import { MyPromise } from "../promise.all";
 
-// function test() {
-//   try {
-//     MyPromise.all(1)
-//       .then((res) => console.log(res))
-//       .catch((err) => console.log("reject", err));
-//   } catch (error) {
-//     console.log("catch", error);
-//   }
-
-//   try {
-//     MyPromise.all({})
-//       .then((res) => console.log(res))
-//       .catch((err) => console.log("reject", err));
-//   } catch (error) {
-//     console.log("catch", error);
-//   }
-
-//   try {
-//     MyPromise.all([])
-//       .then((res) => console.log("success", res))
-//       .catch((err) => console.log("reject", err));
-//   } catch (error) {
-//     console.log("catch", error);
-//   }
-
-//   MyPromise.all([Promise.resolve(1), {}])
-//     .then((res) => console.log("success", res))
-//     .catch((err) => console.log("reject", err));
-
-//   MyPromise.all([Promise.reject("12312321"), Promise.resolve(123)])
-//     .then((res) => console.log("success", res))
-//     .catch((err) => console.log("reject", err));
-// }
-
-// test();
-
 function promiseTest(successCb, errorCb) {
   MyPromise.all([Promise.reject("12312321"), Promise.resolve(123)])
     .then((data) => successCb(data))
